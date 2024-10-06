2ce2702770068baa586b3d3dd8856a04131e0d17
gjsgjs <3466816220@qq.com>
2024-10-06 21:32:24 +0800

    yes

--- hello.py
+++ hello.py
@@ -4,6 +4,7 @@ def welcome_message():
     print("          欢迎使用本程序！          ")
     print("====================================")
     print("try to change with label")
+    print("the catcher in the rye")
 
 if __name__ == "__main__":
     welcome_message()
